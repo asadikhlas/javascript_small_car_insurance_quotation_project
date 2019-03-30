989b61b6fa0eb147ba24f7b63fd4816f0f3062c3
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-30 21:47:26 +0500

    commit

--- insurance/js/app.js
+++ insurance/js/app.js
@@ -1,12 +1,31 @@
+
+
 //Variables
 
-// Event Listeners
-document.addEventListener("DOMContentLoaded", function() {
-  // Create the <option> for the years
-  const html = new HTMLUI();
-  html.displayYears();
-});
+const form = document.getElementById('request-quote')
 
+// Event Listeners
+eventListener();
+function eventListener(){
+    document.addEventListener("DOMContentLoaded", function() {
+        // Create the <option> for the years
+        const html = new HTMLUI();
+        html.displayYears();
+      });
+      
+      
+      //When the form is submitted
+      form.addEventListener('submit',function(e){
+          e.preventDefault();
+          const make = document.getElementById('make').value;
+          const year = document.getElementById('year').value;
+
+          console.log(make)
+          console.log(year)
+
+      })
+
+}
 //Objects
 
 function HTMLUI() {}
@@ -16,18 +35,19 @@ function HTMLUI() {}
 HTMLUI.prototype.displayYears = function() {
   //Max and Min years
   const max = new Date().getFullYear(),
-    min = max - 20;
-  //   console.log(min);
-  //   console.log(max)
+  min = max - 20;
+//   console.log(min);
+//   console.log(max)
 
-  const selectYears = document.getElementById("year");
+const selectYears = document.getElementById('year');
 
-  //print the value
+//print the value
 
-  for (let i = max; i > min; i--) {
-    const option = document.createElement("option");
+for(let i= max; i > min; i--){
+    const option = document.createElement('option')
     option.value = i;
     option.textContent = i;
-    selectYears.appendChild(option);
-  }
+    selectYears.appendChild(option)
+}
+
 };
