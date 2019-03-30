5e41f3e3e3857b7dca397f176bf879144e7be30a
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-30 22:01:25 +0500

    commit

--- insurance/js/app.js
+++ insurance/js/app.js
@@ -1,43 +1,37 @@
-
-
 //Variables
 
-const form = document.getElementById('request-quote');
+const form = document.getElementById("request-quote");
 const html = new HTMLUI();
 
 // Event Listeners
 eventListener();
-function eventListener(){
-    document.addEventListener("DOMContentLoaded", function() {
-        // Create the <option> for the years
-      
-        html.displayYears();
-      });
-      
-      
-      //When the form is submitted
-      form.addEventListener('submit',function(e){
-          e.preventDefault();
+function eventListener() {
+  document.addEventListener("DOMContentLoaded", function() {
+    // Create the <option> for the years
 
+    html.displayYears();
+  });
 
-          // Read the values from the form
-          const make = document.getElementById('make').value;
-          const year = document.getElementById('year').value;
-          //Read the Radio button
-          const level = document.querySelector('input[name="level"]:checked').value 
+  //When the form is submitted
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-          //Checked that all the fields have something
-          if(make === '' || year === '' || level === ''){
-           html.displayError('All the fields are mandatory')
-          }else{
-              console.log('Alright')
-          }
+    // Read the values from the form
+    const make = document.getElementById("make").value;
+    const year = document.getElementById("year").value;
+    //Read the Radio button
+    const level = document.querySelector('input[name="level"]:checked').value;
 
-        //   console.log(make)
-        //   console.log(year)
+    //Checked that all the fields have something
+    if (make === "" || year === "" || level === "") {
+      html.displayError("All the fields are mandatory");
+    } else {
+      console.log("Alright");
+    }
 
-      })
-
+    //   console.log(make)
+    //   console.log(year)
+  });
 }
 //Objects
 
@@ -48,43 +42,38 @@ function HTMLUI() {}
 HTMLUI.prototype.displayYears = function() {
   //Max and Min years
   const max = new Date().getFullYear(),
-  min = max - 20;
-//   console.log(min);
-//   console.log(max)
+    min = max - 20;
+  //   console.log(min);
+  //   console.log(max)
 
-const selectYears = document.getElementById('year');
+  const selectYears = document.getElementById("year");
 
-//print the value
+  //print the value
 
-for(let i= max; i > min; i--){
-    const option = document.createElement('option')
+  for (let i = max; i > min; i--) {
+    const option = document.createElement("option");
     option.value = i;
     option.textContent = i;
-    selectYears.appendChild(option)
-}
-
+    selectYears.appendChild(option);
+  }
 };
-
 
 //Print an Erro
 
-HTMLUI.prototype.displayError = function(message){
-    //Create a div
-    const div = document.createElement('div');
-    div.classList = 'error';
+HTMLUI.prototype.displayError = function(message) {
+  //Create a div
+  const div = document.createElement("div");
+  div.classList = "error";
 
-    //insert the message
-    div.innerHTML = `
+  //insert the message
+  div.innerHTML = `
     <p>${message}</p>
     `;
-    form.insertBefore(div, document.querySelector('.form-group'));
+  form.insertBefore(div, document.querySelector(".form-group"));
 
-    //Remove the error
+  //Remove the error
 
-    setTimeout(function(){
-        document.querySelector('.error').remove()
-    },3000)
-
-
-
-}
+  setTimeout(function() {
+    document.querySelector(".error").remove();
+  }, 3000);
+};
